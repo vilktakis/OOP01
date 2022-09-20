b654fb208ec4de490fb5efadbe7efc002a9e79b1
Skirmantas Auškalnis <79916263+vilktakis@users.noreply.github.com>
2022-09-20 09:37:58 +0300

    Update AgeLogic.cs

--- Logic/AgeLogic.cs
+++ Logic/AgeLogic.cs
@@ -35,7 +35,7 @@ namespace Logic
         {
             if (DateTime.Now.Month < person.DateOfBirth.Month)
             {
-                DateTime nextBD = new DateTime(DateTime.Now.Year+1, person.DateOfBirth.Month, person.DateOfBirth.Day);
+                DateTime nextBD = new DateTime(DateTime.Now.Year, person.DateOfBirth.Month, person.DateOfBirth.Day);
                 return (nextBD - DateTime.Now).Days+1;
             }
             else if (DateTime.Now.Month == person.DateOfBirth.Month && DateTime.Now.Day < person.DateOfBirth.Day)
